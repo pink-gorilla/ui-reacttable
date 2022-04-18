f431b78965eaef49e49a418ed0dee8fcec1e60f6
awb99 <andreas.wolfgang.bauer@gmail.com>
2022-04-18 07:58:55 +0200

    script fix

---
+++ scripts/copy_res.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+cpr () {
+  sourcedir="node_modules/$1"
+  source="$sourcedir/$2"
+  targetdir="target/node_modules/public/$1" 
+  if [ -d $sourcedir ]; then
+     echo "copying $source ==> $targetdir"
+     mkdir -p $targetdir
+     cp $source $targetdir
+  else 
+    echo "ERROR: $sourcedir does not exist."
+  fi
+}
+
+#cpr "ag-grid-community/dist/styles" "*.css"
+
